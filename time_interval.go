66c6368fed759297c990b5023011884546f2240f
Dustin Oprea <myselfasunder@gmail.com>
2017-02-03 13:05:38 -0500

    Added time-interval sort and search.

---
+++ time_interval.go
@@ -0,0 +1,210 @@
+package timeindex
+
+import (
+    "time"
+    "sort"
+    "fmt"
+)
+
+type TimeInterval [2]time.Time
+
+// TimeIntervalSlice Stores a list of two-tuples, representing "from" and "to" 
+// times, sorted by the first (almost identical to TimeSlice but more 
+// convenient).
+type TimeIntervalSlice []TimeInterval
+
+func (tis TimeIntervalSlice) Len() int {
+    return len(tis)
+}
+
+func (tis TimeIntervalSlice) Less(i, j int) bool {
+    return tis[i][0].Before(tis[j][0])
+}
+
+func (tis TimeIntervalSlice) Swap(i, j int) {
+    tis[i], tis[j] = tis[j], tis[i]
+}
+
+// Sort is a convenience method.
+func (tis TimeIntervalSlice) Sort() {
+    sort.Sort(tis)
+}
+
+func (tis TimeIntervalSlice) search(ti TimeInterval) int {
+    return SearchTimeIntervals(tis, ti)
+}
+
+// Search Return all intervals that contain the given time.
+func (tis TimeIntervalSlice) Search(t time.Time) (matches []TimeInterval) {
+    matches = make([]TimeInterval, 0)
+
+    len_ := len(tis)
+    if len_ == 0 {
+        return matches
+    }
+
+    // If there is more than one match, this will point to the first.
+    i := SearchStartTimes(tis, t)
+
+    if i >= len_ {
+        // We're using the search to find a starting point in the list. It doesn't
+        // have to be perfect. Therefore, if we're past the end, move to the left 
+        // by one.
+
+        i--
+    } else {
+        // The interval we're pointing at might have a start-time equal to what 
+        // we're searching for. If there is more than one then step to the end 
+        // (we need to return all intervals that match).
+
+        for ; i < (len_ - 1); i++ {
+            if tis[i][0].After(t) {
+                break
+            }
+        }
+    }
+
+    // This won't run if no matches (i == len_).
+    for ; i >= 0; i-- {
+        if tis[i][0].After(t) {
+            // We're too far right in the list (the whole interval is greater 
+            // than our query).
+
+            continue
+        } else if tis[i][1].Before(t) {
+            // We've moved too far left in the list (the whole intervals is 
+            // smaller than our query).
+
+            break
+        }
+
+        // We're working our way to the front of the sorted list, so prepend 
+        // the results (so that we maintain order).
+        matches = append([]TimeInterval { tis[i] }, matches...)
+    }
+    
+    return matches
+}
+
+func (tis TimeIntervalSlice) getInsertLocation(ti TimeInterval) (i int) {
+    len_ := len(tis)
+    if len_ == 0 {
+        return 0
+    }
+
+    i = tis.search(ti)
+
+    // We were told to append.
+    if i >= len_ {
+        last := tis[len_- 1]
+
+        // Either the new entry's start-time is greater than the start-time of 
+        // the last element or they're equal and the new-entry's stop-time is 
+        // greater.
+        if last[0].Before(ti[0]) || last[0] == ti[0] && last[1].Before(ti[1]) {
+            return len_
+        }
+
+        // Else, step to the left (to a valid entry rather than being past the 
+        // existing list) so that we can do the comparisons below.
+        i--
+    }
+
+    // We were told to insert in the middle of the list.
+    for ; i > 0; i-- {
+        if tis[i][0] == ti[0] {
+            // The current entry's start-time matches the start-time of the new
+            // entry.
+
+            if tis[i][1] == ti[1] {
+                // The current entry's stop-time matches the stop-time of the 
+                // new-entry. Entry already exists.
+
+                return -1
+            } else if tis[i][1].Before(ti[1]) {
+                // The current entry's stop-time is less thanthe new entry's 
+                // stop-time. Insert after.
+
+                return i + 1
+            }
+        } else if tis[i][0].Before(ti[0]) {
+            // The current entry's start-time is less-than the start-time of 
+            // the new entry (the search will only return an `i` of an entry 
+            // that is greater-than-or-equal-to the entry to be added).
+
+            return i + 1
+        }
+    }
+
+    first := tis[0]
+
+    if first == ti {
+        // The first entry is identical.
+
+        return -1
+    } else if first[0].Before(ti[0]) || first[0] == ti[0] && first[1].Before(ti[1]) {
+        // Either the first entry in the list has a start-time that's less 
+        // than the new-entry's or the start-times are equal and the new-
+        // entry's stop-time is greater.
+
+        return 1
+    }
+
+    // If we get here, all elements in [0, `i`] had the same start-time and 
+    // greater stop-times. Insert at the beginning.
+    return 0
+}
+
+func (tis TimeIntervalSlice) Add(ti TimeInterval) (newTis TimeIntervalSlice, err error) {
+    if ti[0].Before(ti[1]) == false {
+        err = fmt.Errorf("interval is invalid")
+
+        return nil, err
+    }
+
+    i := tis.getInsertLocation(ti)
+
+    // Already exists.
+    if i == -1 {
+        return tis, nil
+    }
+
+    right := append(TimeIntervalSlice { ti }, tis[i:]...)
+    newTis = append(tis[:i], right...)
+
+    return newTis, nil
+}
+
+func SearchTimeIntervals(tis TimeIntervalSlice, ti TimeInterval) int {
+    p := func(i int) bool {
+        return tis[i][0].After(ti[0]) || ti == tis[i]
+    }
+    
+    return search(len(tis), p)
+}
+
+func SearchStartTimes(tis TimeIntervalSlice, t time.Time) int {
+    p := func(i int) bool {
+        return tis[i][0].After(t) || t == tis[i][0]
+    }
+    
+    return search(len(tis), p)
+}
+
+func search(n int, f func(int) bool) int {
+    // Define f(-1) == false and f(n) == true.
+    // Invariant: f(i-1) == false, f(j) == true.
+    i, j := 0, n
+    for i < j {
+        h := i + (j-i)/2 // avoid overflow when computing h
+        // i ≤ h < j
+        if !f(h) {
+            i = h + 1 // preserves f(i-1) == false
+        } else {
+            j = h // preserves f(j) == true
+        }
+    }
+
+    // i == j, f(i-1) == false, and f(j) (= f(i)) == true  =>  answer is i.
+    return i
+}
---
+++ time_interval_test.go
@@ -0,0 +1,448 @@
+package timeindex
+
+import (
+    "testing"
+    "time"
+    "fmt"
+
+    "github.com/dsoprea/go-logging"
+)
+
+func TestTimeIntervalAddDuplicate(t *testing.T) {
+    left1, err := time.Parse(time.RFC3339, "2016-12-03T07:23:50Z")
+    log.PanicIf(err)
+
+    right1, err := time.Parse(time.RFC3339, "2016-12-04T07:23:50Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+
+    tis := make(TimeIntervalSlice, 0)
+    
+    tis, err = tis.Add(ti1)
+    log.PanicIf(err)
+
+    tis, err = tis.Add(ti1)
+    log.PanicIf(err)
+
+    if len(tis) != 1 {
+        t.Fatalf("Duplicate added.")
+    } else if tis[0] != ti1 {
+        t.Fatalf("TIS is out of order (with duplicate?).")
+    }
+}
+
+func checkTestAddOrder(description string, t *testing.T, unsortedIntervals []TimeInterval, sortedIntervals []TimeInterval) {
+    if len(unsortedIntervals) != len(sortedIntervals) {
+        log.Panic(fmt.Errorf("unsorted and sorted intervals do not have the same length."))
+    }
+
+    tis := make(TimeIntervalSlice, 0)
+
+    var err error
+    for _, ti := range unsortedIntervals {
+        tis, err = tis.Add(ti)
+        log.PanicIf(err)
+    }
+
+    len1 := len(tis)
+    len2 := len(unsortedIntervals)
+    if len1 != len2 {
+        log.Panic(fmt.Errorf("%s: Size of new slice does not equal size of dataset: (%d) != (%d)", description, len1, len2))
+    }
+
+    for i, ti := range sortedIntervals {
+        if tis[i] != ti {
+            t.Errorf("%s: Slice out of order at (%d).\n", description, i)
+            t.Errorf("  UNSORTED: %v", unsortedIntervals)
+            t.Errorf("    SORTED: %v", sortedIntervals)
+            t.Errorf("    ACTUAL: %v", tis)
+
+            log.Panic(fmt.Errorf("Slice not sorted after add."))
+        }
+    }
+}
+
+func TestTimeIntervalAddDifferentStartTimes(t *testing.T) {
+    left1, err := time.Parse(time.RFC3339, "2016-12-03T07:23:50Z")
+    log.PanicIf(err)
+
+    left2, err := time.Parse(time.RFC3339, "2016-12-03T07:24:50Z")
+    log.PanicIf(err)
+
+    left3, err := time.Parse(time.RFC3339, "2016-12-03T07:25:50Z")
+    log.PanicIf(err)
+
+    right1, err := time.Parse(time.RFC3339, "2016-12-04T07:23:50Z")
+    log.PanicIf(err)
+
+    right2, err := time.Parse(time.RFC3339, "2016-12-08T07:23:50Z")
+    log.PanicIf(err)
+
+    right3, err := time.Parse(time.RFC3339, "2016-12-06T07:23:50Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+    ti2 := TimeInterval { left2, right2 }
+    ti3 := TimeInterval { left3, right3 }
+
+    sortedIntervals := []TimeInterval { ti1, ti2, ti3 }
+
+    checkTestAddOrder("1", t, []TimeInterval { ti1, ti2, ti3 }, sortedIntervals)
+    checkTestAddOrder("2", t, []TimeInterval { ti1, ti3, ti2 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti2, ti1, ti3 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti2, ti3, ti1 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti3, ti2, ti1 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti3, ti1, ti2 }, sortedIntervals)
+}
+
+func TestTimeIntervalAddSameStartTime(t *testing.T) {
+    left1, err := time.Parse(time.RFC3339, "2016-12-03T07:23:50Z")
+    log.PanicIf(err)
+/*
+    left2, err := time.Parse(time.RFC3339, "2016-12-03T07:24:50Z")
+    log.PanicIf(err)
+
+    left3, err := time.Parse(time.RFC3339, "2016-12-03T07:25:50Z")
+    log.PanicIf(err)
+*/
+    right1, err := time.Parse(time.RFC3339, "2016-12-04T07:23:50Z")
+    log.PanicIf(err)
+
+    right2, err := time.Parse(time.RFC3339, "2016-12-06T07:23:50Z")
+    log.PanicIf(err)
+
+    right3, err := time.Parse(time.RFC3339, "2016-12-08T07:23:50Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+    ti2 := TimeInterval { left1, right2 }
+    ti3 := TimeInterval { left1, right3 }
+
+    sortedIntervals := []TimeInterval { ti1, ti2, ti3 }
+
+    checkTestAddOrder("1", t, []TimeInterval { ti1, ti2, ti3 }, sortedIntervals)
+    checkTestAddOrder("2", t, []TimeInterval { ti1, ti3, ti2 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti3, ti2, ti1 }, sortedIntervals)
+    checkTestAddOrder("4", t, []TimeInterval { ti2, ti3, ti1 }, sortedIntervals)
+}
+
+func TestTimeIntervalAddSameAndDifferentStartTimes(t *testing.T) {
+    // Minutes are different.
+
+    left1, err := time.Parse(time.RFC3339, "2016-12-03T07:23:50Z")
+    log.PanicIf(err)
+
+    left2, err := time.Parse(time.RFC3339, "2016-12-03T07:24:50Z")
+    log.PanicIf(err)
+
+    left3, err := time.Parse(time.RFC3339, "2016-12-03T07:25:50Z")
+    log.PanicIf(err)
+
+    left4, err := time.Parse(time.RFC3339, "2016-12-03T07:26:50Z")
+    log.PanicIf(err)
+
+    left5, err := time.Parse(time.RFC3339, "2016-12-03T07:27:50Z")
+    log.PanicIf(err)
+
+    // Days are different.
+
+    right1, err := time.Parse(time.RFC3339, "2016-12-04T07:23:50Z")
+    log.PanicIf(err)
+
+    right2, err := time.Parse(time.RFC3339, "2016-12-06T07:23:50Z")
+    log.PanicIf(err)
+
+    right3, err := time.Parse(time.RFC3339, "2016-12-08T07:23:50Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+
+    ti2 := TimeInterval { left2, right1 }
+    ti3 := TimeInterval { left3, right2 }
+    ti4 := TimeInterval { left4, right3 }
+
+    ti5 := TimeInterval { left5, right1 }
+
+    sortedIntervals := []TimeInterval { ti1, ti2, ti3, ti4, ti5 }
+
+    // Keep the entries with the same start-times together and in order.
+    checkTestAddOrder("1", t, []TimeInterval { ti1, ti2, ti3, ti4, ti5 }, sortedIntervals)
+    checkTestAddOrder("2", t, []TimeInterval { ti2, ti3, ti4, ti5, ti1 }, sortedIntervals)
+    checkTestAddOrder("3", t, []TimeInterval { ti5, ti1, ti2, ti3, ti4 }, sortedIntervals)
+
+    // Move the entries around the same as before, but shuffle the ones with 
+    // the same start-time.
+
+    checkTestAddOrder("5", t, []TimeInterval { ti1, ti2, ti4, ti3, ti5 }, sortedIntervals)
+    checkTestAddOrder("6", t, []TimeInterval { ti1, ti3, ti2, ti4, ti5 }, sortedIntervals)
+    checkTestAddOrder("7", t, []TimeInterval { ti1, ti3, ti4, ti2, ti5 }, sortedIntervals)
+    checkTestAddOrder("8", t, []TimeInterval { ti1, ti4, ti2, ti3, ti5 }, sortedIntervals)
+    checkTestAddOrder("9", t, []TimeInterval { ti1, ti4, ti3, ti2, ti5 }, sortedIntervals)
+
+    checkTestAddOrder("11", t, []TimeInterval { ti2, ti4, ti3, ti5, ti1 }, sortedIntervals)
+    checkTestAddOrder("12", t, []TimeInterval { ti3, ti2, ti4, ti5, ti1 }, sortedIntervals)
+    checkTestAddOrder("13", t, []TimeInterval { ti3, ti4, ti2, ti5, ti1 }, sortedIntervals)
+    checkTestAddOrder("14", t, []TimeInterval { ti4, ti2, ti3, ti5, ti1 }, sortedIntervals)
+    checkTestAddOrder("15", t, []TimeInterval { ti4, ti3, ti2, ti5, ti1 }, sortedIntervals)
+
+    checkTestAddOrder("17", t, []TimeInterval { ti5, ti1, ti2, ti4, ti3 }, sortedIntervals)
+    checkTestAddOrder("18", t, []TimeInterval { ti5, ti1, ti3, ti2, ti4 }, sortedIntervals)
+    checkTestAddOrder("19", t, []TimeInterval { ti5, ti1, ti3, ti4, ti2 }, sortedIntervals)
+    checkTestAddOrder("20", t, []TimeInterval { ti5, ti1, ti4, ti2, ti3 }, sortedIntervals)
+    checkTestAddOrder("21", t, []TimeInterval { ti5, ti1, ti4, ti3, ti2 }, sortedIntervals)
+}
+
+func TestTimeIntervalSearchSimple(t *testing.T) {
+    left1, err := time.Parse(time.RFC3339, "2016-12-03T07:23:50Z")
+    log.PanicIf(err)
+
+    left2, err := time.Parse(time.RFC3339, "2016-12-05T07:24:50Z")
+    log.PanicIf(err)
+
+    left3, err := time.Parse(time.RFC3339, "2016-12-07T07:25:50Z")
+    log.PanicIf(err)
+
+    right1, err := time.Parse(time.RFC3339, "2016-12-04T07:23:50Z")
+    log.PanicIf(err)
+
+    right2, err := time.Parse(time.RFC3339, "2016-12-06T07:24:50Z")
+    log.PanicIf(err)
+
+    right3, err := time.Parse(time.RFC3339, "2016-12-08T07:25:50Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+    ti2 := TimeInterval { left2, right2 }
+    ti3 := TimeInterval { left3, right3 }
+
+    tis := make(TimeIntervalSlice, 0)
+    
+    tis, err = tis.Add(ti1)
+    log.PanicIf(err)
+
+    tis, err = tis.Add(ti2)
+    log.PanicIf(err)
+
+    tis, err = tis.Add(ti3)
+    log.PanicIf(err)
+
+    i := tis.search(ti1)
+    if i != 0 {
+        t.Fatalf("Search for item in entry 0 failed.")
+    }
+
+    i = tis.search(ti2)
+    if i != 1 {
+        t.Fatalf("Search for item in entry 1 failed.")
+    }
+
+    i = tis.search(ti3)
+    if i != 2 {
+        t.Fatalf("Search for item in entry 2 failed.")
+    }
+}
+
+func searchTestIntervals(description string, t *testing.T, intervals []TimeInterval, q time.Time, expectedMatches []TimeInterval) {
+    tis := make(TimeIntervalSlice, 0)
+
+    var err error
+    for _, ti := range intervals {
+        tis, err = tis.Add(ti)
+        log.PanicIf(err)
+    }
+
+    matches := tis.Search(q)
+
+    len1 := len(matches)
+    len2 := len(expectedMatches)
+    if len1 != len2 {
+        log.Panic(fmt.Errorf("Match count is not expected: (%d) != (%d)", len1, len2))
+    }
+
+    for i, ti := range matches {
+        if ti != expectedMatches[i] {
+            t.Errorf("%s: Match at (%d) unexpected: [%s] != [%s]", description, i, ti, expectedMatches[i])
+            t.Errorf("  CURRENT: %v", matches)
+            t.Errorf("  EXPECTED: %v", expectedMatches)
+
+            t.Fatalf("Matches not unexpected.")
+        }
+    }
+}
+
+func TestTimeIntervalSearchComplex(t *testing.T) {
+    left1, err := time.Parse(time.RFC3339, "2016-01-01T02:00:00Z")
+    log.PanicIf(err)
+
+    left2, err := time.Parse(time.RFC3339, "2016-01-01T02:15:00Z")
+    log.PanicIf(err)
+
+    left3, err := time.Parse(time.RFC3339, "2016-01-01T02:45:00Z")
+    log.PanicIf(err)
+
+    left4, err := time.Parse(time.RFC3339, "2016-01-02T02:00:00Z")
+    log.PanicIf(err)
+
+    left5, err := time.Parse(time.RFC3339, "2016-01-02T02:00:00Z")
+    log.PanicIf(err)
+
+    left6, err := time.Parse(time.RFC3339, "2016-01-02T02:00:00Z")
+    log.PanicIf(err)
+
+    left7, err := time.Parse(time.RFC3339, "2016-01-03T02:00:00Z")
+    log.PanicIf(err)
+
+    left8, err := time.Parse(time.RFC3339, "2016-01-03T02:00:00Z")
+    log.PanicIf(err)
+
+    left9, err := time.Parse(time.RFC3339, "2016-01-03T02:00:00Z")
+    log.PanicIf(err)
+
+    right1, err := time.Parse(time.RFC3339, "2016-01-01T04:00:00Z")
+    log.PanicIf(err)
+
+    right2, err := time.Parse(time.RFC3339, "2016-01-01T06:00:00Z")
+    log.PanicIf(err)
+
+    right3, err := time.Parse(time.RFC3339, "2016-01-01T08:00:00Z")
+    log.PanicIf(err)
+
+    right4, err := time.Parse(time.RFC3339, "2016-01-02T04:00:00Z")
+    log.PanicIf(err)
+
+    right5, err := time.Parse(time.RFC3339, "2016-01-02T06:00:00Z")
+    log.PanicIf(err)
+
+    right6, err := time.Parse(time.RFC3339, "2016-01-02T08:00:00Z")
+    log.PanicIf(err)
+
+    right7, err := time.Parse(time.RFC3339, "2016-01-03T04:00:00Z")
+    log.PanicIf(err)
+
+    right8, err := time.Parse(time.RFC3339, "2016-01-03T06:00:00Z")
+    log.PanicIf(err)
+
+    right9, err := time.Parse(time.RFC3339, "2016-01-03T08:00:00Z")
+    log.PanicIf(err)
+
+    ti1 := TimeInterval { left1, right1 }
+    ti2 := TimeInterval { left2, right2 }
+    ti3 := TimeInterval { left3, right3 }
+    ti4 := TimeInterval { left4, right4 }
+    ti5 := TimeInterval { left5, right5 }
+    ti6 := TimeInterval { left6, right6 }
+    ti7 := TimeInterval { left7, right7 }
+    ti8 := TimeInterval { left8, right8 }
+    ti9 := TimeInterval { left9, right9 }
+
+    intervals := []TimeInterval { ti1, ti2, ti3, ti4, ti5, ti6, ti7, ti8, ti9 }
+
+    //// Generic tests.
+
+    // Search before all intervals.
+
+    q, err := time.Parse(time.RFC3339, "2016-01-01T00:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("1", t, intervals, q, []TimeInterval {})
+
+    // Search between two intervals (hitting on neither).
+
+    q, err = time.Parse(time.RFC3339, "2016-01-01T09:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("2", t, intervals, q, []TimeInterval {})
+
+    // Search after all intervals.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-03T09:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("3", t, intervals, q, []TimeInterval {})
+
+    //// Tests within the first three intervals.
+
+    // Search within the first three intervals.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-01T02:10:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("4", t, intervals, q, []TimeInterval { ti1 })
+
+    // Search for the left side of the first three intervals.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-01T02:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("5", t, intervals, q, []TimeInterval { ti1 })
+
+    // Search for the right side of the first three interval.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-01T08:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("6", t, intervals, q, []TimeInterval { ti3 })
+
+    // Search such that first three intervals (with staggered start-times) will 
+    // be returned.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-01T03:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("7", t, intervals, q, []TimeInterval { ti1, ti2, ti3 })
+
+    // Search such that first three intervals (with identical start-times) will
+    // be returned.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-02T02:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("8", t, intervals, q, []TimeInterval { ti4, ti5, ti6 })
+
+    //// Tests within the second set of three intervals.
+
+    // Search in the middle.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-02T03:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("9", t, intervals, q, []TimeInterval { ti4, ti5, ti6 })
+
+    // Search for the left side.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-02T02:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("10", t, intervals, q, []TimeInterval { ti4, ti5, ti6 })
+
+    // Search for the right side.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-02T08:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("11", t, intervals, q, []TimeInterval { ti6 })
+
+    //// Tests within the third set of three intervals.
+
+    // Search in the middle.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-03T03:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("12", t, intervals, q, []TimeInterval { ti7, ti8, ti9 })
+
+    // Search for the left side.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-03T02:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("13", t, intervals, q, []TimeInterval { ti7, ti8, ti9 })
+
+    // Search for the right side.
+
+    q, err = time.Parse(time.RFC3339, "2016-01-03T08:00:00Z")
+    log.PanicIf(err)
+
+    searchTestIntervals("14", t, intervals, q, []TimeInterval { ti9 })
+}
